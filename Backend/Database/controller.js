a02a6abc839f2456919c24811aaed7adf32528a2
Justin <justingilberto2000@gmail.com>
2023-09-03 14:53:51 -0600

    feat-fix-add-restrictions

--- Backend/Database/controller.js
+++ Backend/Database/controller.js
@@ -4,13 +4,33 @@ Calling SP Login
 */
 const login = async(req, res) => 
 {
+    const patron = /^[a-zA-Z0-9._%+-]+@estudiantec\.cr$/;
+
     const pool = await conn.getConnection();
-    const result = await pool.request()
-        .input('inUser', req.body.inUser)
+    var result = await pool.request()
+        .input('inName', req.body.inName)
         .input('inPassword', req.body.inPassword)
         .output('outLoginSuccess', 0)             
         .output('outResultCode', 0)
-        .execute('LoginDB');
+        if(patron.test(req.body.inName)){
+            result = await pool.request()
+            .input('inName', req.body.inName)
+            .input('inPassword', req.body.inPassword)
+            .output('outLoginSuccess', 0)             
+            .output('outResultCode', 0)
+            .execute('LoginDB')
+            console.log('Estudiante')
+        }else{
+            result = await pool.request()
+            .input('inName', req.body.inName)
+            .input('inPassword', req.body.inPassword)
+            .output('outLoginSuccess', 0)             
+            .output('outResultCode', 0)
+            .execute('LoginDBAso');
+            console.log('Asocia')
+        }
+        console.log(result.output.outLoginSuccess)
+        console.log(result.output.outResultCode)
         if (result.output.outResultCode == 0 && result.output.outLoginSuccess == 0){
             res.json({
                 access: "Login Exitoso",
@@ -27,8 +47,7 @@ const login = async(req, res) =>
 /*
 Calling SP SignUp
 */
-const register = async(req, res) => 
-{
+const registerStudent = async (req, res) => {
     const pool = await conn.getConnection();
     const result = await pool.request()
         .input('inName', req.body.inName)
@@ -37,9 +56,9 @@ const register = async(req, res) =>
         .input('inLicence', req.body.inLicence)
         .input('inPassword', req.body.inPassword)
         .input('inUserType', req.body.inUserType)
-        .input('inPhone', req.boby.inPhone)
-        .input('inCareer', req.boby.inCareer)
-        .input('inID', req.boby.inID)
+        .input('inPhone', req.body.inPhone)
+        .input('inCareer', req.body.inCareer)
+        .input('inID', req.body.inID)
         .output('outResultCode', 0)
         .execute('SignUpDB');
     if (result.output.outResultCode == 50001) {
@@ -59,6 +78,38 @@ const register = async(req, res) =>
         })
     }
 };
+
+/*
+Calling SP SignUpAsociations
+*/
+const registerAsociation = async(req, res) => 
+{
+    const pool = await conn.getConnection();
+    const result = await pool.request()
+    .input('inName', req.body.inName)
+    .input('inPassword', req.body.inPassword)
+    .input('inCareer', req.body.inCareer)
+    .output('outResultCode', 0)
+    .execute('SignUpBDAso');
+    if (result.output.outResultCode == 50001) {
+        res.json({
+            access: "Registro fallido",
+            message: "La asociación ya éxiste"
+        })
+    } else if (result.output.outResultCode == 0){
+        res.json({
+            access: "Registro exitoso",
+            message: "Se registró con éxito"
+        })
+    } else {
+        res.json({
+            access: "Registro fallido",
+            message: "Verifique que los datos"
+        })
+    }
+};
+
 exports.login = login;
-exports.register = register;
+exports.registerStudent = registerStudent;
+exports.registerAsociation = registerAsociation;
 
